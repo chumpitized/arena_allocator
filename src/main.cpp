407c6f4087ab544bda0f823d1c29f296c81d9e5a
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-02-21 12:23:39 -0600

    add arena alloc function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
+#include <cstring>
 #include <iostream>
+
+//Ginger Bill implementation...
+
+static unsigned char *arena_buffer;
+static size_t arena_buffer_length;
+static size_t arena_offset;
+
+void *arena_alloc(size_t size) {
+	// Check to see if the backing memory has space left
+	if (arena_offset + size <= arena_buffer_length) {
+		void *ptr = &arena_buffer[arena_offset];
+		arena_offset += size;
+
+		// Zero new memory by default
+		memset(ptr, 0, size);
+		return ptr;
+	}
+	// Return NULL if the arena is out of memory
+	return NULL;
+}
 
 int main() {
 
 	std::cout << "memory allocator" << std::endl;
+	std::cout << 2 * sizeof(void *) << std::endl;
 	
 	return 1;
 }
