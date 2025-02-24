88ae5309344e08547b7621f56d82e1126af31004
chumpitized <126842502+chumpitized@users.noreply.github.com>
2025-02-24 09:49:42 -0600

    align to any power of two

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
+#include <assert.h>
 #include <cstring>
 #include <iostream>
 
 //Ginger Bill implementation...
-
 
 static unsigned char arena_buffer[256];
 static size_t arena_buffer_length = 256;
@@ -12,17 +12,42 @@ bool is_power_of_two(uintptr_t x) {
 	return (x & (x-1)) == 0;
 }
 
-void *arena_alloc(size_t size) {
-	// Check to see if the backing memory has space left
-	if (arena_offset + size <= arena_buffer_length) {
+uintptr_t align_forward(uintptr_t ptr, size_t align) {
+	assert(is_power_of_two(align));
+
+	uintptr_t p, a, modulo;
+
+	p = ptr;
+	a = (uintptr_t)align;
+	
+	//Example: 
+	//p = 0x08; 0b00001000;
+	//a = 0x10; 0b00010000;
+	//p & (a - 1);
+	//=
+	//0b00001000 & (0b00001111);
+	//=
+	//0b00001000;
+	//shift forward by 8 bits / 1 byte to align to address 16;
+	modulo = p & (a - 1);
+
+	if (modulo != 0) {
+		// If 'p' address is not aligned, push the address to the
+		// next value which is aligned
+		p += a - modulo;
+	}
+	return p;
+}
+
+void *arena_alloc(size_t size) {	
+	if (arena_offset + size <= arena_buffer_length) {		
 		void *ptr = &arena_buffer[arena_offset];
 		arena_offset += size;
 
-		// Zero out new memory by default
 		memset(ptr, 0, size);
 		return ptr;
 	}
-	// Return NULL if the arena is out of memory
+
 	return NULL;
 }
 
@@ -30,11 +55,14 @@ int main() {
 
 	void *ptr = arena_alloc(1);
 	//memset(ptr, 255, 1);
-	*(uint8_t *) ptr = 256;
+	//*(uint8_t *) ptr = 255;
+	*(char *) ptr = 'c';
 
-	uint8_t val = *(uint8_t *)ptr;
+	//uint8_t val = *(uint8_t *)ptr;
+	char val = *(char *)ptr;
 
-	std::cout << "The Value: " << +val << std::endl;
+	//std::cout << "The Value: " << +val << std::endl;
+	std::cout << "The Value: " << val << std::endl;
 	std::cout << arena_offset << std::endl;
 	
 	return 1;
